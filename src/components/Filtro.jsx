197042f1b3e6563ef772db71aae6e0e6ceffb131
Felipe <fejemogu@gmail.com>
2026-09-23 14:56:58 -0300

    modifiqué los botones del filtro para que no se vea compacto

--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -1,6 +1,6 @@
 function Filtro({ cambiarFiltro }) {
   return (
-    <div className="mb-4 space-x-2">
+    <div className="flex overflow-x-auto gap-2 mb-4 pb-2">
       <button onClick={() => cambiarFiltro("Todos")} className="bg-gray-300 p-2 rounded">Todos</button>
       <button onClick={() => cambiarFiltro("Platos")} className="bg-gray-300 p-2 rounded">Platos</button>
       <button onClick={() => cambiarFiltro("Extras")} className="bg-gray-300 p-2 rounded">Extras</button>
