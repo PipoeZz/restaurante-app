8ad465d8b54dde1c15403be2a3501f4d266612bd
Inzunzaaa <franciscoinzunza1324@gmail.com>
2026-09-23 16:42:31 -0300

    diseño horizontal y cambio de colores

--- src/components/MenuCarta.jsx
+++ src/components/MenuCarta.jsx
@@ -1,21 +1,25 @@
 function MenuCarta({ plato, agregar }) {
   return (
-    <div className="border p-4 bg-white rounded shadow-sm flex flex-col justify-between">
-      <div>
-        <img src={plato.imagen} alt={plato.nombre} className="w-full h-40 object-cover rounded-md mb-4" />
-        <h3 className="font-bold text-lg">{plato.nombre}</h3>
-        <p className="text-green-600 font-bold mb-2">${plato.precio}</p>
-        
-        <p className="text-sm text-gray-600 mb-4">{plato.detalle}</p>
-      </div>
+    <div className="bg-gray-200 text-black p-4 rounded shadow-sm flex justify-between items-center">
 
-      <button 
+      <div className="flex-1 pr-4">
+        <h3 className="font-bold text-lg">{plato.nombre}</h3>
+        <p className="font-bold mb-2">${plato.precio}</p>
+        <p className="text-sm text-gray-700 mb-3">{plato.detalle}</p>
+        
+       <button 
         onClick={() => agregar(plato)} 
-        className="bg-blue-500 text-white p-2 w-full rounded hover:bg-blue-600 font-bold mt-auto"
+        className="bg-black text-white px-4 py-1.5 rounded hover:bg-gray-800 text-sm font-bold"
       >
         Agregar al Carrito
       </button>
     </div>
+    <img
+      src={plato.imagen}
+      alt={plato.nombre}
+      className="w-24 h-24 object-cover rounded-md"
+    />
+  </div>  
   );
 }
 
